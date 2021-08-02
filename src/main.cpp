f1d6b708534cc92f5bb06fbbddd02cd50fde489c
panyi <525647740@qq.com>
2021-08-02 20:43:02 +0800

    add mac

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main(int argc , char *argv[]) {
     std::string imagePath =  argv[1];
 
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
